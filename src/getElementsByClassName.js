4ce2fd172f555f2719830fcd7a6e8d3413a36ada
b0char0va <b0char0va.mona@gmail.com>
2018-09-17 15:18:44 -0700

    Initial commit, passing getElementsByClassName and stringifyJSON
    TODO:
    PARSE JSON!

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,16 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var result = [];
+  var getElement = function(node) {
+    if (node.classList && node.classList.contains(className)) {
+      result.push(node);
+    }
+    for (var i = 0; i < node.childNodes.length; i++) {
+      getElement(node.childNodes[i]);
+    }
+    return result;
+  };
+  return getElement(document.body);
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,53 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+/*
+If the value has a toJSON() method, it's responsible to define what data will be serialized.
+Boolean, Number, and String objects are converted to the corresponding primitive values during stringification, in accord with the traditional conversion semantics.
+If undefined, a Function, or a Symbol is encountered during conversion it is either omitted (when it is found in an object) or censored to null (when it is found in an array). JSON.stringify() can also just return undefined when passing in "pure" values like JSON.stringify(function(){}) or JSON.stringify(undefined).
+All Symbol-keyed properties will be completely ignored, even when using the replacer function.
+The instances of Date implement the toJSON() function by returning a string (the same as date.toISOString()). Thus, they are treated as strings.
+The numbers Infinity and NaN, as well as the object null, are all considered null.
+All the other Object instances (including Map, Set, WeakMap, and WeakSet) will have only their enumerable properties serialized.
+*/
+
+  if (obj === true) {
+    return 'true';
+  } else if (obj === false) {
+    return 'false';
+  } else if (typeof(obj) === 'number') {
+    return String(obj);
+  } else if (typeof(obj) === 'string') {
+    return '"' + obj + '"';
+  } else if (obj === null) {
+    return 'null';
+  } else if (Array.isArray(obj)) {
+    var returnString = '';
+    for (var i = 0; i < obj.length; i++) {
+      returnString += stringifyJSON(obj[i]) + ',';
+    }
+
+    if (returnString[returnString.length - 1] === ',') {
+      returnString = returnString.slice(0, returnString.length - 1);
+    }
+    return '[' + returnString + ']';
+  } else if (typeof(obj) === 'object') { // its an object
+    var returnString = '';
+    for (var key in obj) {
+      if (key === 'functions' || key === 'undefined') {
+        returnString += '';
+      } else {
+        returnString += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+      }
+    }
+
+    if (returnString[returnString.length - 1] === ',') {
+      returnString = returnString.slice(0, returnString.length - 1);
+    }
+    return '{' + returnString + '}';   
+  } else if (typeof(obj) === 'function' || obj === undefined) {
+    return '';
+  }
+
 };
+
