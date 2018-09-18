944f41a451526a8a5a093b9a81dd2ba210bfeb0d
b0char0va <b0char0va.mona@gmail.com>
2018-09-18 15:37:48 -0700

    Adding Lisa's fork and adding parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -1,7 +1,56 @@
 // this is what you would do if you were one to do things the easy way:
 // var parseJSON = JSON.parse;
 
-// but you're not, so you'll write it from scratch:
+  function charCount(str) {
+  var characterDict = {'\'': 0, '[': 0, ']': 0, '{' : 0, '}': 0, '"': 0};
+  for (var i = 0; i < json.length; i++) {
+    if (json[i] in characterDict) {
+      characterDict[json[i]]++;
+    }
+  }
+
+  if (characterDict['\'']%2 !== 0) {
+    return false;
+  } else if (characterDict['"']%2 !== 0) {
+    return false;
+  } else if(characterDict['['] !== characterDict[']']) {
+    return false;
+  } else if(characterDict['{'] !== characterDict['}']) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
 var parseJSON = function(json) {
   // your code goes here
+  if (json[0] === '[' && json[json.length - 1] === ']') {
+    if (json.length === 2) {
+      return [];
+    }
+
+    var result = [];
+    var index = 1;
+    for (var i = 1; i < json.length; i++) {
+        if (json[i] === ',') {
+          var subString = json.substring(index, i);
+          if(charCount(substring))
+        }
+    }
+
+  } else if (json === 'true') {
+      return true;
+  } else if (json === 'false') {
+    return false;
+  } else if (typeof(json.substring(1, json.length - 1)) === 'string') {
+    return json.substring(1, json.length-1);
+  } else if (Number(json) !== NaN) {
+    return Number(json);
+  } 
 };
+
+
+
+console.log(parseJSON('["one", "two"]'));
+
+
